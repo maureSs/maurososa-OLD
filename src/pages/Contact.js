fd13636ea910b4e77431708ce2ca340cd28dd70f
maureS <mauroaf4@gmail.com>
2021-04-28 09:22:33 -0300

    website commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavbarComponent from '../components/NavbarComponent';
 import emailjs from 'emailjs-com';
 
 export default function Contact() {
+  const [showForm, setShowForm] = useState(false);
+
   function sendEmail(e) {
     e.preventDefault();
 
@@ -29,35 +31,44 @@ export default function Contact() {
     <div className='main-container'>
       <NavbarComponent />
       <div className='contact-container'>
-        <form onSubmit={sendEmail}>
-          <div className='form-control'>
-            <label>
-              <h4 className='extra'>Your Name</h4>
-              <input type='text' placeholder='Name' name='name' />
-            </label>
-            <label>
-              <h4 className='extra'>Your Email</h4>
-              <input type='email' placeholder='Email Address' name='email' />
-            </label>
-            <label>
-              <h4 className='extra'>Subject</h4>
-              <input type='text' placeholder='Subject' name='subject' />
-            </label>
-            <label>
-              <h4 className='extra'>Your Message</h4>
-              <textarea
-                className='message-input'
-                placeholder='Your message'
-                name='message'
-              ></textarea>
-            </label>
-            <input
-              className='submit-btn'
-              type='submit'
-              value='Send Message'
-            ></input>
-          </div>
-        </form>
+        {showForm ? (
+          <form onSubmit={sendEmail}>
+            <div className='form-control'>
+              <label>
+                <h4 className='extra'>Your Name</h4>
+                <input type='text' placeholder='Name' name='name' />
+              </label>
+              <label>
+                <h4 className='extra'>Your Email</h4>
+                <input type='email' placeholder='Email Address' name='email' />
+              </label>
+              <label>
+                <h4 className='extra'>Subject</h4>
+                <input type='text' placeholder='Subject' name='subject' />
+              </label>
+              <label>
+                <h4 className='extra'>Your Message</h4>
+                <textarea
+                  className='message-input'
+                  placeholder='Your message'
+                  name='message'
+                ></textarea>
+              </label>
+              <input
+                className='submit-btn'
+                type='submit'
+                value='Send Message'
+              ></input>
+            </div>
+          </form>
+        ) : (
+          <button
+            className='open-form-button'
+            onClick={() => setShowForm(!showForm)}
+          >
+            SEND ME AN EMAIL!
+          </button>
+        )}
       </div>
     </div>
   );

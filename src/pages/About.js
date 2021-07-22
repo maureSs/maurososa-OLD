55dab725cd2d0d3fdfe01c423eee8692a8c83318
maureS <mauroaf4@gmail.com>
2021-07-22 17:50:20 -0300

    website commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,10 +9,15 @@ export default function About() {
       <div className='about-container'>
         Hey Hello!
         <br />
-        My name is Mauro and I am a highly capable, Front-end developer from
-        Buenos Aires, Argentina. I love learning new technologies and being able
-        to implement them in my projects, in order to create fun and useful
-        applications.
+        My name is Mauro, I am 22 years old and I am a highly capable front-end
+        developer from Buenos Aires, Argentina. I have been in the programming
+        world for several years, but in 2019 my interest in web development
+        began. In 2020 I started developing my first web applications, using
+        React.js. Since then I have learned new tools and technologies and have
+        also improved on what I already knew. I aspire to be a software
+        developer in the future. Over time I realized that I love learning new
+        technologies and being able to implement them in my projects, in order
+        to create fun and useful applications.
         <div>
           <img src={hacker} alt='' />
         </div>

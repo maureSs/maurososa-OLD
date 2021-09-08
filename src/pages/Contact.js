d6ef8c08b265f7a2b431125765682c5e906bc51d
maureS <mauroaf4@gmail.com>
2021-09-07 22:44:45 -0300

    website commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
+// components
 import NavbarComponent from '../components/NavbarComponent';
+// emailjs
 import emailjs from 'emailjs-com';
+// icons
+import { RiCloseLine } from 'react-icons/ri';
 
 export default function Contact() {
   const [showForm, setShowForm] = useState(false);
@@ -34,6 +38,10 @@ export default function Contact() {
         {showForm ? (
           <form onSubmit={sendEmail}>
             <div className='form-control'>
+              <RiCloseLine
+                className='cancel-button'
+                onClick={() => setShowForm(!showForm)}
+              />
               <label>
                 <h4 className='extra'>Your Name</h4>
                 <input type='text' placeholder='Name' name='name' />
@@ -62,24 +70,45 @@ export default function Contact() {
             </div>
           </form>
         ) : (
-          <div className='button-wrapper'>
-            <div className='link-wrapper'>
-              <button
-                className='open-form-button'
-                onClick={() => setShowForm(!showForm)}
-              >
-                SEND ME AN EMAIL!
-              </button>
-              <div className='icon'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  viewBox='0 0 268.832 268.832'
+          <>
+            <div className='button-wrapper'>
+              <div className='link-wrapper'>
+                <button
+                  className='open-form-button'
+                  onClick={() => setShowForm(!showForm)}
                 >
-                  <path d='M265.17 125.577l-80-80c-4.88-4.88-12.796-4.88-17.677 0-4.882 4.882-4.882 12.796 0 17.678l58.66 58.66H12.5c-6.903 0-12.5 5.598-12.5 12.5 0 6.903 5.597 12.5 12.5 12.5h213.654l-58.66 58.662c-4.88 4.882-4.88 12.796 0 17.678 2.44 2.44 5.64 3.66 8.84 3.66s6.398-1.22 8.84-3.66l79.997-80c4.883-4.882 4.883-12.796 0-17.678z' />
-                </svg>
+                  CONTACT FORM
+                </button>
+                <div className='icon'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 268.832 268.832'
+                  >
+                    <path d='M265.17 125.577l-80-80c-4.88-4.88-12.796-4.88-17.677 0-4.882 4.882-4.882 12.796 0 17.678l58.66 58.66H12.5c-6.903 0-12.5 5.598-12.5 12.5 0 6.903 5.597 12.5 12.5 12.5h213.654l-58.66 58.662c-4.88 4.882-4.88 12.796 0 17.678 2.44 2.44 5.64 3.66 8.84 3.66s6.398-1.22 8.84-3.66l79.997-80c4.883-4.882 4.883-12.796 0-17.678z' />
+                  </svg>
+                </div>
               </div>
             </div>
-          </div>
+            <div className='button-wrapper' style={{ marginTop: 120 }}>
+              <div className='link-wrapper'>
+                <a
+                  href='mailto:maurososa118@gmail.com'
+                  className='open-form-button'
+                  style={{ fontSize: 15 }}
+                >
+                  SEND ME AN EMAIL!
+                </a>
+                <div className='icon'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 268.832 268.832'
+                  >
+                    <path d='M265.17 125.577l-80-80c-4.88-4.88-12.796-4.88-17.677 0-4.882 4.882-4.882 12.796 0 17.678l58.66 58.66H12.5c-6.903 0-12.5 5.598-12.5 12.5 0 6.903 5.597 12.5 12.5 12.5h213.654l-58.66 58.662c-4.88 4.882-4.88 12.796 0 17.678 2.44 2.44 5.64 3.66 8.84 3.66s6.398-1.22 8.84-3.66l79.997-80c4.883-4.882 4.883-12.796 0-17.678z' />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </>
         )}
       </div>
     </div>
